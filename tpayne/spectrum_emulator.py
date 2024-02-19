be6f8721280106d500b7bcd16dac4a3b3421700e
Maja Jablonska <majajjablonska@gmail.com>
2024-02-20 10:26:55 +1100

    Add docstrings

--- tpayne/spectrum_emulator.py
+++ tpayne/spectrum_emulator.py
@@ -9,28 +9,66 @@ T = TypeVar("T")
 class SpectrumEmulator(Generic[T]):
     @classproperty
     def label_names(cls) -> List[str]:
+        """Get labels of spectrum model parameters
+
+        Returns:
+            List[str]:
+        """
         return []
     
     @classproperty
     def min_parameters(cls) -> T:
+        """Minimum values supported by the spectrum model
+
+        Returns:
+            T:
+        """
         return np.array([])
     
     @classproperty
     def max_parameters(cls) -> T:
+        """Maximum values supported by the spectrum model
+
+        Returns:
+            T:
+        """
         return np.array([])
     
     @classmethod
     def is_in_bounds(cls, parameters: T) -> bool:
+        """Check if parameters are within the bounds of the spectrum model
+
+        Args:
+            parameters (T):
+
+        Returns:
+            bool:
+        """
         return np.all(parameters >= cls.min_parameters) and np.all(parameters <= cls.max_parameters)
     
     @classproperty
     def solar_parameters(cls) -> T:
+        """Solar parameters for the spectrum model
+
+        Returns:
+            T:
+        """
         return np.array([])
 
     @classmethod
     def to_parameters(cls) -> T:
+        """Convert passed values to the accepted parameters format
+
+        Returns:
+            T:
+        """
         return np.array([])
     
     @classproperty
     def flux_method(cls) -> Callable[..., T]:
+        """Get method for simulating flux for passed parameters
+
+        Returns:
+            Callable[..., T]:
+        """
         return lambda x: x
--- tpayne/tpayne.py
+++ tpayne/tpayne.py
@@ -26,30 +26,75 @@ except ImportError:
 class TPayne(SpectrumEmulator[ArrayLike]):
     @classproperty
     def label_names(cls) -> List[str]:
+        """Get labels of spectrum model parameters
+
+        Returns:
+            List[str]:
+        """
         return const.LABEL_NAMES
     
     @classproperty
     def min_parameters(cls) -> ArrayLike:
+        """Minimum values supported by the spectrum model
+
+        Returns:
+            ArrayLike:
+        """
         return MIN_PARAMS
     
     @classproperty
     def max_parameters(cls) -> ArrayLike:
+        """Maximum values supported by the spectrum model
+
+        Returns:
+            ArrayLike:
+        """
         return MAX_PARAMS
     
     @classmethod
     def is_in_bounds(cls, parameters: ArrayLike) -> bool:
+        """Check if parameters are within the bounds of the spectrum model
+
+        Args:
+            parameters (ArrayLike):
+
+        Returns:
+            bool:
+        """
         return jnp.all(parameters >= const.MIN_PARAMS) and jnp.all(parameters <= const.MAX_PARAMS)
     
     @classproperty
     def solar_parameters(cls) -> ArrayLike:
+        """Solar parameters for the spectrum model
+
+        Returns:
+            ArrayLike:
+        """
         return SOLAR_PARAMS
     
     @classmethod
     def to_parameters(cls,
-                      logteff: float = const.SOLAR_LOG_TEFF,
-                      logg: float = const.SOLAR_LOGG,
+                      logteff: float = 3.7617023675414125,
+                      logg: float = 4.44,
                       mu: float = 1.0,
                       abundances: Union[ArrayLike, Dict[str, float]] = const.SOLAR_ABUNDANCES) -> ArrayLike:
+        """Convert passed values to the accepted parameters format
+
+        Args:
+            logteff (float, optional): log10(effective temperature) [log(K)]. Defaults to solar log(teff)=3.7617023675414125.
+            logg (float, optional): log10(g) [log(m/s2)]. Defaults to solar logg=4.44.
+            mu (float, optional): [0.0-1.0]. Defaults to 1.0.
+            abundances (Union[ArrayLike, Dict[str, float]], optional): abundances relative to the solar abundance 
+                passed either as an array or dict in the form of {'element name': 0.0} (refer to label names).
+                Defaults to solar abundances (all elements=0.0).
+
+        Raises:
+            ValueError: when the parameters are out of accepted bounds
+
+        Returns:
+            ArrayLike:
+        """
+        
         if isinstance(abundances, dict):
             abundance_values = jnp.array([abundances.get(element, 0.) for element in const.ELEMENTS])
         else:
